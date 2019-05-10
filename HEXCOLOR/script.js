6961034b724b87a61a1db3c3d3ccc2867d5ee8e7
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-10 01:58:52 +0000

    used the random code to generate colors

---
+++ HEXCOLOR/script.js
@@ -0,0 +1,21 @@
+//var
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hexBtn = document.querySelector('.hexBtn');
+const bodyBcg = document.querySelector('body');
+const hex = document.querySelector('.hex'); //from <span> where will be placed the color-code
+
+hexBtn.addEventListener('click', getHex);
+
+function getHex() {
+    let hexCol = '#';
+
+    for (let i = 0; i < 6; i++) {
+        let random = Math.floor(Math.random() * hexNumbers.length);
+        //console.log(random);
+        hexCol += hexNumbers[random]; // will add the '#' to letters and numbers randomly.
+        //console.log(hexCol);
+        
+    }
+    bodyBcg.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
+}
